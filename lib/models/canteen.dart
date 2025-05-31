9d0453a4b546810fbd43b9d32e31e36b2e17c33e
Xingchen722 <e1459295@u.nus.edu>
2025-05-31 23:57:23 +0800

    提交信息

--- lib/models/canteen.dart
+++ lib/models/canteen.dart
@@ -5,6 +5,8 @@ class Canteen {
   final String image;
   final String description;
   final String operatingHours;
+  final double rating;
+  final List<String> categories;
 
   Canteen({
     required this.id,
@@ -13,6 +15,8 @@ class Canteen {
     required this.image,
     required this.description,
     required this.operatingHours,
+    this.rating = 0.0,
+    this.categories = const [],
   });
 
   factory Canteen.fromJson(Map<String, dynamic> json) {
@@ -23,6 +27,8 @@ class Canteen {
       image: json['image'] as String,
       description: json['description'] as String,
       operatingHours: json['operatingHours'] as String,
+      rating: (json['rating'] as num?)?.toDouble() ?? 0.0,
+      categories: (json['categories'] as List<dynamic>?)?.cast<String>() ?? [],
     );
   }
 
@@ -34,6 +40,8 @@ class Canteen {
       'image': image,
       'description': description,
       'operatingHours': operatingHours,
+      'rating': rating,
+      'categories': categories,
     };
   }
 } 
